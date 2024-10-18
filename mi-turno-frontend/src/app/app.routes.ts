66b63ce4800b7244c22d24318f55cae8a173f6a0
Leo-Dev <leonardocaimmi1@gmail.com>
2024-10-18 19:31:51 -0300

    ruta global

--- mi-turno-frontend/src/app/app.routes.ts
+++ mi-turno-frontend/src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing/landing-page/landing-page.component';
+import { LoginComponent } from './pages/inicioSesion/login/login.component';
 
 export const routes: Routes = [{
   path:"landing-page",
+  component:LandingPageComponent
+},{
+  path:"login",
+  component:LoginComponent
+},{
+  path:"**",
   component:LandingPageComponent
 }];
