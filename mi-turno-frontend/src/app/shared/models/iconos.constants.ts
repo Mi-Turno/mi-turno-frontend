78f597d11acb971a6c50f708e7e917129426c5f7
Fabri-dev <fabriziogolisciano@estudiante.mdp.utn.edu.ar>
2024-10-19 16:31:56 -0300

    se agrego const iconos

---
+++ mi-turno-frontend/src/app/shared/models/iconos.constants.ts
@@ -0,0 +1,11 @@
+export const ICONOS =
+{
+   email:"email",
+   person:"person",
+   signature:"signature",
+   call:"call",
+   iconoContrasenia:{ abierto:"visibility",
+  cerrado:"visibility_off"
+ },
+  home: 'home',
+}
