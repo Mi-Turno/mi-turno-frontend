88a5b220867a8c2eb71b111cb828b2ce57937961
Leo-Dev <leonardocaimmi1@gmail.com>
2024-10-19 12:08:06 -0300

    input con iconos

--- mi-turno-frontend/src/app/pages/inicioSesion/login/login.component.ts
+++ mi-turno-frontend/src/app/pages/inicioSesion/login/login.component.ts
@@ -1,11 +1,11 @@
 import { Component, ElementRef, EventEmitter, ViewChild } from '@angular/core';
 import { InputComponent } from "../../../shared/components/input/input.component";
-
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [InputComponent],
+  imports: [InputComponent,MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
