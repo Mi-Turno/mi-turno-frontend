4bd545ea4fbb7cd4900ad9cac47997e7c8687fa4
Fabri-dev <fabriziogolisciano@estudiante.mdp.utn.edu.ar>
2024-10-19 13:01:37 -0300

    componente register con sus rutas

--- mi-turno-frontend/src/app/app.routes.ts
+++ mi-turno-frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing/landing-page/landing-page.component';
 import { LoginComponent } from './pages/inicioSesion/login/login.component';
+import { RegisterComponent } from './pages/inicioSesion/register/register.component';
 
 export const routes: Routes = [{
   path:"landing-page",
@@ -8,7 +9,12 @@ export const routes: Routes = [{
 },{
   path:"login",
   component:LoginComponent
-},{
+},
+{
+  path:"register",
+  component:RegisterComponent
+},
+{
   path:"**",
   component:LandingPageComponent
 }];
