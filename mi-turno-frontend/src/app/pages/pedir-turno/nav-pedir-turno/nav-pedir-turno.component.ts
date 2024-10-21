124397e6c90b7e03d6c15e8267ea559ed2bc855c
Fabri-dev <fabriziogolisciano@estudiante.mdp.utn.edu.ar>
2024-10-21 11:38:10 -0300

    logica http usuario implementada

---
+++ mi-turno-frontend/src/app/pages/pedir-turno/nav-pedir-turno/nav-pedir-turno.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-pedir-turno',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-pedir-turno.component.html',
+  styleUrl: './nav-pedir-turno.component.css'
+})
+export class NavPedirTurnoComponent {
+
+}
