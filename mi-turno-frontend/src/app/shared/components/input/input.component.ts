16451cad8f577c05cbec72a5eb7470a64d791c7e
Leo-Dev <leonardocaimmi1@gmail.com>
2024-10-21 22:07:19 -0300

    agregue la logica de formularios reactivos
    modifique la logica para poder usar controladores de los formularios

--- mi-turno-frontend/src/app/shared/components/input/input.component.ts
+++ mi-turno-frontend/src/app/shared/components/input/input.component.ts
@@ -1,30 +1,70 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Inject, Input, Output } from '@angular/core';
+import { Component, EventEmitter, forwardRef, Input, Output } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [CommonModule,MatIcon],
+  imports: [CommonModule, InputComponent,MatIcon, ReactiveFormsModule,FormsModule],
   templateUrl: './input.component.html',
-  styleUrl: './input.component.css'
+  styleUrls: ['./input.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ]
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
 
 @Input()
 id?: string;
 @Input()
 type: string = "text";
 @Input()
-placeholder: string= "Ingrese aqui..."
+placeholder: string= "Ingrese aqui...";
 @Input()
 claseInputContainer: string = "inputContainer";
 @Input()
 textoLabel:string="Texto predeterminado";
 @Input()
 icono:string="sin icono";
+
 // Emitir el evento cuando se hace click
 
+private _value: string = '';
+
+// Métodos de ControlValueAccessor
+onChange = (value: any) => {};
+onTouched = () => {};
+
+set value(val: string) {
+  this._value = val;
+  this.onChange(val);
+  this.onTouched();
+}
+
+get value(): string {
+  return this._value;
+}
+
+writeValue(value: any): void {
+  this.value = value ? value : '';
+}
+
+registerOnChange(fn: any): void {
+  this.onChange = fn;
+}
+
+registerOnTouched(fn: any): void {
+  this.onTouched = fn;
+}
+
+// Opción para deshabilitar el componente
+setDisabledState?(isDisabled: boolean): void {
+}
 
 
 
