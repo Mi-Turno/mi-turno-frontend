4fd08f76783690a222e71eb8a91b2ad3eb09334c
facundoprocelli <128741375+facundoprocelli@users.noreply.github.com>
2024-10-23 19:08:40 -0300

    Creación de nav Bar de local
    Cree el Nav bar de local
    Cree el componente chip
    Cree el componente fecha y hora
    Agregue un tamaño de letra
    Ajuste tamaño de fuente en el sidebar

--- mi-turno-frontend/src/app/pages/dashboardLocal/nav-bar/nav-bar.component.ts
+++ mi-turno-frontend/src/app/pages/dashboardLocal/nav-bar/nav-bar.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { FechaYHoraComponent } from '../../../shared/components/fecha-y-hora/fecha-y-hora.component';
+import { ChipComponent } from "../../../shared/components/chip/chip.component";
+import { ICONOS } from '../../../shared/models/iconos.constants';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatIcon, FechaYHoraComponent, ChipComponent],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+
+iconos = ICONOS;
+texto = "Nuevo Turno";
+referencia = '#';
 
 }
