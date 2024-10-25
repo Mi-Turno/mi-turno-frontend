49a11d5ce22e931cb8c8164c7b134f2a5346f4d9
Leo-Dev <leonardocaimmi1@gmail.com>
2024-10-24 21:11:18 -0300

    metodo para obtener usuario mediante email y su contraseña

--- mi-turno-frontend/src/app/core/services/usuarioService/usuario.service.ts
+++ mi-turno-frontend/src/app/core/services/usuarioService/usuario.service.ts
@@ -1,4 +1,4 @@
-import { HttpBackend, HttpClient } from '@angular/common/http';
+import { HttpBackend, HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UsuarioInterface } from '../../interfaces/usuario-interface';
@@ -22,6 +22,12 @@ export class UsuarioService {
   };
   public getUsuarios(): Observable<UsuarioInterface[]>{
     return this.http.get<UsuarioInterface[]>(this.urlBase);
+  }
+  public getUsuariosByEmailAndPassword(email:string,password:string): Observable<UsuarioInterface>{
+    const params = new HttpParams()
+    .set('email',email)
+    .set('password',password);
+    return this.http.get<UsuarioInterface>(this.urlBase,{params,responseType:"json"});
   }
   public postUsuario(usuario:UsuarioInterface):Observable<UsuarioInterface>{
     return this.http.post<UsuarioInterface>(this.urlBase,usuario)
