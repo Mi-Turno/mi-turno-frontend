c0837e617e780ccff4f1593a602be3e98f9259a4
facundoprocelli <128741375+facundoprocelli@users.noreply.github.com>
2024-10-26 20:13:24 -0300

    update
    creacion de componente horario

--- mi-turno-frontend/src/app/pages/dashboardLocal/horarios/horarios.component.ts
+++ mi-turno-frontend/src/app/pages/dashboardLocal/horarios/horarios.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { ChipComponent } from '../../../shared/components/chip/chip.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-horarios',
   standalone: true,
-  imports: [],
+  imports: [MatIcon, ChipComponent,CommonModule,FormsModule, MatSlideToggleModule],
   templateUrl: './horarios.component.html',
   styleUrl: './horarios.component.css'
 })
 export class HorariosComponent {
 
+  horarios: string[] = ['10:00', '11:00', '12:00', '13:00', '14:00'];
+  toggleActivo: boolean = false;
+
+  // Función para alternar el estado del toggle
+  cambiarEstadoToggle() {
+    this.toggleActivo = !this.toggleActivo;
+  }
+
+  // Función para añadir un nuevo horario
+  agregarHorario() {
+    const nuevoHorario = prompt('Ingresa un nuevo horario (HH:mm):');
+    if (nuevoHorario) {
+      this.horarios.push(nuevoHorario);
+    }
+  }
+
+  // Función para eliminar un horario específico
+  eliminarHorario(index: number) {
+    this.horarios.splice(index, 1);
+  }
 }
