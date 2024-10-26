88665e7a647555f47a2790c70ad4731f63c31b08
Fabri-dev <fabriziogolisciano@estudiante.mdp.utn.edu.ar>
2024-10-26 19:13:58 -0300

    pedir-turno avances

--- mi-turno-frontend/src/app/pages/pedirTurno/pedir-turno/pedir-turno.component.ts
+++ mi-turno-frontend/src/app/pages/pedirTurno/pedir-turno/pedir-turno.component.ts
@@ -2,15 +2,27 @@ import { Component } from '@angular/core';
 import { NavPedirTurnoComponent } from "../nav-pedir-turno/nav-pedir-turno.component";
 import { NavPasosComponent } from "../nav-pasos/nav-pasos.component";
 import { CardComponent } from "../../../shared/components/card/card.component";
+import { TurnoInterface } from '../../../core/interfaces/turno-interface';
+import { MetodosDePago } from '../../../shared/models/metodosDePago';
+import { ConfirmacionComponent } from '../confirmacion/confirmacion.component';
 
 @Component({
   selector: 'app-pedir-turno',
   standalone: true,
-  imports: [NavPedirTurnoComponent, NavPasosComponent, CardComponent],
+  imports: [NavPedirTurnoComponent, NavPasosComponent, CardComponent,ConfirmacionComponent],
   templateUrl: './pedir-turno.component.html',
   styleUrl: './pedir-turno.component.css'
 })
 export class PedirTurnoComponent {
+
+  turno:TurnoInterface = {
+    idUsuario:1, //se obtiene mediante LocalStorage del JWT
+    idServicio:1, //se obtiene mediante el servicio de servicios
+    idProfesional:"1",
+    fecha:new Date(),
+    horaInicio:new Date(),
+    metodoPago: MetodosDePago.efectivo
+  }
 
   pasoActual: number = 1; // Variable que controla el paso actual
 
@@ -20,6 +32,7 @@ export class PedirTurnoComponent {
     if (this.pasoActual < 4) {
       this.pasoActual++;
     }
+    console.log(this.turno);
   }
 
   // Función para retroceder al paso anterior
