8c107c4f1b4f0207f403b8864370164e108722a2
Leo-Dev <leonardocaimmi1@gmail.com>
2024-10-26 13:45:34 -0300

    codigos de error http

---
+++ mi-turno-frontend/src/app/shared/models/httpError.constants.ts
@@ -0,0 +1,19 @@
+export const codigoErrorHttp = {
+NO_ENCONTRADO:404,
+CREADO:201,
+ERROR_SERVIDOR:500,
+ERROR_CONTACTAR_SERVIDOR:0
+}
+/*error: (error: HttpErrorResponse) => {
+  if (error.status === codigoErrorHttp.NO_ENCONTRADO) {
+    alert('Error 404: Usuario no encontrado');
+
+  } else if (error.status === codigoErrorHttp.ERROR_SERVIDOR) {
+    alert('Error 500: Error del servidor');
+
+  } else if (error.status === codigoErrorHttp.ERROR_CONTACTAR_SERVIDOR) {
+    alert('Error de conexión: No se pudo contactar con el servidor (ERR_CONNECTION_REFUSED)');
+  } else {
+    alert('Error inesperado. Intente otra vez mas tarde.');
+  }
+}*/
