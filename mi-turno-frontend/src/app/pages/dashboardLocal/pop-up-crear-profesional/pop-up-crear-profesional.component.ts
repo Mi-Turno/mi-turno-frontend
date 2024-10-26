44f5c04d1acd943fbe2d7edd08eb8943c7fdc329
facundoprocelli <128741375+facundoprocelli@users.noreply.github.com>
2024-10-26 15:56:56 -0300

    UPDATE
    Cree las constantes de roles
    Conecté la búsqueda por rol con el back
    Avancé con el formulario reactivo de crear profesional

--- mi-turno-frontend/src/app/pages/dashboardLocal/pop-up-crear-profesional/pop-up-crear-profesional.component.ts
+++ mi-turno-frontend/src/app/pages/dashboardLocal/pop-up-crear-profesional/pop-up-crear-profesional.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { BotonComponent } from '../../../shared/components/boton/boton.component';
 import { InputComponent } from '../../../shared/components/input/input.component';
 import { MatIcon } from '@angular/material/icon';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ICONOS } from '../../../shared/models/iconos.constants';
 import { PLACEHOLDERS } from '../../../shared/models/placeholderInicioSesion.constants';
+import { UsuarioInterface } from '../../../core/interfaces/usuario-interface';
+import { ROLES } from '../../../shared/models/rolesUsuario.constants';
+import { UsuarioService } from '../../../core/services/usuarioService/usuario.service';
 
 @Component({
   selector: 'app-pop-up-crear-profesional',
@@ -16,22 +19,84 @@ import { PLACEHOLDERS } from '../../../shared/models/placeholderInicioSesion.con
 })
 export class PopUpCrearProfesionalComponent {
 
+  roles = ROLES;
 icono = ICONOS;
 placeholder = PLACEHOLDERS;
 tipoPopUp = 'profesionales'
+usuarioService = inject(UsuarioService);
 
 @Input() fotoProfesional = "img-default.png"
 
 
 formularioRegister = new FormGroup ({
-
   nombre: new FormControl('', Validators.required),
   apellido: new FormControl('', Validators.required),
   email: new FormControl('', [Validators.required, Validators.email]),
+  fechaNacimiento: new FormControl('',Validators.required),
   telefono: new FormControl('', Validators.required),
-  fechaNacimiento: new FormControl('', Validators.required),
-
 });
+
+
+
+crearUnProfesional():UsuarioInterface {
+  const nombreForm = this.formularioRegister.get('nombre')?.value ||'';//||'' esto significa que puede ser null
+  const apellidoForm = this.formularioRegister.get('apellido')?.value||'';
+  const emailForm = this.formularioRegister.get('email')?.value||'';
+  const fechaNacimientoForm = this.formularioRegister.get('fechaNacimiento')?.value||'';
+  const telefonoForm = this.formularioRegister.get('telefono')?.value||'';
+  const password = "Programador";
+  const rol = this.roles.profesional;
+  return {
+    nombre:nombreForm,
+    apellido:apellidoForm,
+    email:emailForm,
+    fechaNacimiento:fechaNacimientoForm,
+    telefono:telefonoForm,
+    password:password,
+    rol:rol
+  };
+}
+
+private postUsuarioToBackend(usuario:UsuarioInterface):void{
+  try {
+    this.usuarioService.postUsuario(usuario).subscribe({
+      next:(usuario:UsuarioInterface) =>{
+
+        console.log(usuario);
+      },
+      error:(error)=>{
+        console.error(error);
+      }
+    })
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+onSubmit() {
+console.log(this.formularioRegister.value);
+
+  if (this.formularioRegister.valid) {
+    console.log('Usuario enviado con exito');
+    console.log(this.formularioRegister.value);
+    const usuario:UsuarioInterface = this.crearUnProfesional();
+    console.log(usuario);
+    this.postUsuarioToBackend(usuario);
+  } else {
+    let campoError: string = '';
+    Object.keys(this.formularioRegister.controls).forEach(campo => {
+      const control = this.formularioRegister.get(campo);
+      if (control?.invalid) {
+        campoError += (`${campo} es inválido, `);
+      }
+    });
+    alert(campoError);
+  }
+}
+
+
+
+
 
 @Output() desactivarOverlay: EventEmitter<void> = new EventEmitter<void>();
 
