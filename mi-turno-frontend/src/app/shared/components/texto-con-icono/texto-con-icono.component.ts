ec95516cc0d972f8307c270f3a288b3dc621f4d4
Fabri-dev <fabriziogolisciano@estudiante.mdp.utn.edu.ar>
2024-10-26 00:43:52 -0300

    cambios logica comp texto-con-icono

--- mi-turno-frontend/src/app/shared/components/texto-con-icono/texto-con-icono.component.ts
+++ mi-turno-frontend/src/app/shared/components/texto-con-icono/texto-con-icono.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-texto-con-icono',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule,CommonModule],
   templateUrl: './texto-con-icono.component.html',
   styleUrl: './texto-con-icono.component.css'
 })
@@ -16,6 +17,9 @@ export class TextoConIconoComponent {
   @Input() claseTexto: string = 'claseTexto';
 
   @Input() href:string = "#";
+  @Input() seleccionado = false;
+
+  claseSeleccionado:string = 'seleccionado';
 
   constructor() { }
 }
