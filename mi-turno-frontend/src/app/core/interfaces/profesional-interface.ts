c3a289248acaba321e321a559e7c18d108128313
Leo-Dev <leonardocaimmi1@gmail.com>
2024-11-08 00:31:36 -0300

    agregue automatizacion de los clientes para la confirmacion

--- mi-turno-frontend/src/app/core/interfaces/profesional-interface.ts
+++ mi-turno-frontend/src/app/core/interfaces/profesional-interface.ts
@@ -1,6 +1,6 @@
-export interface ProfesionalInterface{
-  idProfesional?:number;
-  nombre:string;
-  precioServicio:number;
+import { UsuarioInterface } from "./usuario-interface";
+//todo modificar para tener todos los precios de todos los servicios
+export interface ProfesionalInterface extends UsuarioInterface{
+  precioServicio:number;//mapa de servicios con sus precios
 
 }
