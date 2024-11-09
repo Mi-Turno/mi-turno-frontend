5d753969ce3a0dfc939df9af59ec9db4858c54e8
Leo-Dev <leonardocaimmi1@gmail.com>
2024-11-09 13:28:34 -0300

    cambios

--- mi-turno-frontend/src/app/pages/landing/landing-page/landing-page.component.ts
+++ mi-turno-frontend/src/app/pages/landing/landing-page/landing-page.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-landing/nav-bar.component';
 import { HeroComponent } from '../hero/hero.component';
 import { SobreNosotrosComponent } from '../sobre-nosotros/sobre-nosotros.component';
-import { InputComponent } from "../../../shared/components/input/input.component";
 import { CarouselComponent } from "../carousel/carousel.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavBarComponent, HeroComponent, SobreNosotrosComponent, InputComponent, CarouselComponent],
+  imports: [NavBarComponent, HeroComponent, SobreNosotrosComponent, CarouselComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
