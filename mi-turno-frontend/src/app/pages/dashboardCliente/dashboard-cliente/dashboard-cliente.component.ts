c3fc0776d25b875255e608da572c664848faada7
Fabri-dev <fabriziogolisciano@estudiante.mdp.utn.edu.ar>
2024-11-09 12:00:41 -0300

    agregamos components cliente

--- mi-turno-frontend/src/app/pages/dashboardCliente/dashboard-cliente/dashboard-cliente.component.ts
+++ mi-turno-frontend/src/app/pages/dashboardCliente/dashboard-cliente/dashboard-cliente.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavPedirTurnoComponent } from "../../pedirTurno/nav-pedir-turno/nav-pedir-turno.component";
+import { WidgetBienvenidaComponent } from "../widget-bienvenida/widget-bienvenida.component";
+import { TablaTurnosComponent } from "../tabla-turnos/tabla-turnos.component";
 
 @Component({
   selector: 'app-dashboard-cliente',
   standalone: true,
-  imports: [],
+  imports: [NavPedirTurnoComponent, WidgetBienvenidaComponent, TablaTurnosComponent],
   templateUrl: './dashboard-cliente.component.html',
   styleUrl: './dashboard-cliente.component.css'
 })
--- mi-turno-frontend/src/app/pages/dashboardCliente/widget-bienvenida/widget-bienvenida.component.ts
+++ mi-turno-frontend/src/app/pages/dashboardCliente/widget-bienvenida/widget-bienvenida.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FechaYHoraComponent } from "../../../shared/components/fecha-y-hora/fecha-y-hora.component";
+import { ClienteInterface } from '../../../core/interfaces/cliente-interface';
+import { BotonComponent } from "../../../shared/components/boton/boton.component";
 
 @Component({
   selector: 'app-widget-bienvenida',
   standalone: true,
-  imports: [],
+  imports: [FechaYHoraComponent, BotonComponent],
   templateUrl: './widget-bienvenida.component.html',
   styleUrl: './widget-bienvenida.component.css'
 })
 export class WidgetBienvenidaComponent {
+  @Input() nombreCliente: string = "Nombre";
+  @Input() apellidoCliente: string = "Apellido";
 
 }
