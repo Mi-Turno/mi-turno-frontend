12f5b53af06b4c0bf88d25b56784abfef6812889
facundoprocelli <128741375+facundoprocelli@users.noreply.github.com>
2024-11-13 18:13:58 -0300

    Cambios de estilos en el nav y hero

--- mi-turno-frontend/src/app/app.routes.ts
+++ mi-turno-frontend/src/app/app.routes.ts
@@ -70,3 +70,4 @@ export const routes: Routes = [{
   pathMatch:"full"
 
 }];
+
--- mi-turno-frontend/src/app/pages/landing/nav-landing/nav-bar.component.ts
+++ mi-turno-frontend/src/app/pages/landing/nav-landing/nav-bar.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ICONOS } from '../../../shared/models/iconos.constants';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
+  imports: [MatIconModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 
 export class NavBarComponent {
+
+  iconos = ICONOS;
+
+
 urlBase:string = "landing-page/"
 urlIcono:string = "icono.png";
 heroHref:string =`#hero`
