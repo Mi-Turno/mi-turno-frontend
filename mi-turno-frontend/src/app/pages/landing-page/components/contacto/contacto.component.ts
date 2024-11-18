3021ade577afb2a734f4be4d5fd3cd8b44406dd3
facundoprocelli <128741375+facundoprocelli@users.noreply.github.com>
2024-11-18 20:48:58 -0300

    Bloqueo de envío cuando ya se mandó un mail

--- mi-turno-frontend/src/app/pages/landing-page/components/contacto/contacto.component.ts
+++ mi-turno-frontend/src/app/pages/landing-page/components/contacto/contacto.component.ts
@@ -16,6 +16,9 @@ import { codigoErrorHttp } from '../../../../shared/models/httpError.constants';
 })
 export class ContactoComponent {
 
+
+mailEnviado = false;
+
 formularioContacto = new FormGroup ({
   nombre: new FormControl('', Validators.required),
   negocio: new FormControl('', Validators.required),
@@ -38,7 +41,7 @@ enviarMail() {
   const email: EmailContactoInterface = this.crearEmail();
   this.emailService.postEnviarEmail(email).subscribe({
     next: (response) => {
-      console.log(response);
+      this.bloquearContacto();
     }, error: (error: HttpErrorResponse) => {
       if (error.status === codigoErrorHttp.NO_ENCONTRADO) {
         alert('Error 404: Email no encontrado');
@@ -54,6 +57,12 @@ enviarMail() {
         alert('Error al enviar el Email');
       }
 }})
+}
+
+
+bloquearContacto() {
+  this.mailEnviado = true;
+  alert("Mail envíado de forma correcta");
 }
 
 }
