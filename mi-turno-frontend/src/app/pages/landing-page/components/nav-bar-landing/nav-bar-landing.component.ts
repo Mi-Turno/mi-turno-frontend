4cb2c2939b52b588528c0527dfaef97ff4716122
Leo-Dev <leonardocaimmi1@gmail.com>
2025-01-06 12:46:31 -0300

    navegar por los componentes

--- mi-turno-frontend/src/app/pages/landing-page/components/nav-bar-landing/nav-bar-landing.component.ts
+++ mi-turno-frontend/src/app/pages/landing-page/components/nav-bar-landing/nav-bar-landing.component.ts
@@ -1,24 +1,57 @@
 import { Component } from '@angular/core';
 import { ICONOS } from '../../../../shared/models/iconos.constants';
 import { MatIcon } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar-landing',
   standalone: true,
-  imports: [MatIcon],
+  imports: [MatIcon,],
   templateUrl: './nav-bar-landing.component.html',
-  styleUrl: './nav-bar-landing.component.css'
+  styleUrls: ['./nav-bar-landing.component.css']
 })
 export class NavBarLandingComponent {
   iconos = ICONOS;
 
+  urlBase: string = "landing-page/";
+  urlIcono: string = "icono.png";
+  heroHref: string = `#hero`;
+  sobreNosotrosHref: string = `#introduccion`;
+  beneficioHref: string = `#beneficios`;
+  preciosHref: string = `#precios`;
+  contactoHref: string = `#contacto`;
+  loginHref: string = "login";
 
-urlBase:string = "landing-page/";
-urlIcono:string = "icono.png";
-heroHref:string =`#hero`;
-sobreNosotrosHref:string =`#introduccion`;
-beneficioHref:string = `#beneficios`;
-preciosHref:string = `#precios`;
-contactoHref:string = `#contacto`;
-loginHref:string = "login";
+  constructor(private router: Router) {}
+
+  navegar(sectionId: string): void {
+    const rutaActual = this.router.url.split('#')[0]; //ruta actual
+    const ruta = this.obtenerRutaDesdeLaSeccion(sectionId);
+
+    if (rutaActual !== ruta) {
+      this.router.navigate([ruta]).then(() => {
+        this.deslizar(sectionId);
+      });
+    } else {
+      this.deslizar(sectionId);
+    }
+  }
+
+  private deslizar(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });//deslizar hacia el elemento
+    }
+  }
+
+  private obtenerRutaDesdeLaSeccion(sectionId: string): string {
+    const rutas: { [key: string]: string } = {
+      hero: 'landing-page', //ruta por defecto
+      introduccion: 'landing-page',
+      beneficios: 'landing-page',
+      precios: 'landing-page',
+      contacto: 'landing-page',
+    };
+    return rutas[sectionId] || 'landing-page';
+  }
 }
