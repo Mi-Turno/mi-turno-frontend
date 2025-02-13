1208fced24369ce8fa5fbf72ebe33337370101d1
Leo-Dev <leonardocaimmi1@gmail.com>
2025-02-13 14:51:15 -0300

    comportamiento historial levantado

--- src/app/pages/dashboardCliente/dashboard-cliente.component.ts
+++ src/app/pages/dashboardCliente/dashboard-cliente.component.ts
@@ -1,18 +1,19 @@
 import { TurnoInterface } from '../../core/interfaces/turno-interface';
-import { ChangeDetectorRef, Component, inject, OnInit  } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, inject, OnInit, ViewChild  } from '@angular/core';
 import { NegocioInterface } from '../../core/interfaces/negocio-interface';
 import { NegocioServiceService } from '../../core/services/negocioService/negocio-service.service';
 import { ClienteService } from '../../core/services/clienteService/cliente.service';
 import { ClienteInterface } from '../../core/interfaces/cliente-interface';
 import { CommonModule } from '@angular/common';
 import { WidgetBienvenidaComponent } from './components/widget-bienvenida/widget-bienvenida.component';
-import { TablaTurnosComponent } from './components/tabla-turnos/tabla-turnos.component';
 
 import { ModalComponent } from "../../shared/components/modal/modal.component";
 import { ElegirNegocioComponent } from "./components/elegir-negocio/elegir-negocio.component";
 import { HttpErrorResponse } from '@angular/common/http';
 import { AuthService } from '../../core/guards/auth/service/auth.service';
 import { NavPedirTurnoComponent } from "../pedirTurno/components/nav-pedir-turno/nav-pedir-turno.component";
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter } from 'rxjs';
 
 
 
@@ -21,12 +22,13 @@ import { NavPedirTurnoComponent } from "../pedirTurno/components/nav-pedir-turno
 @Component({
   selector: 'app-dashboard-cliente',
   standalone: true,
-  imports: [CommonModule, WidgetBienvenidaComponent, TablaTurnosComponent, ModalComponent, ElegirNegocioComponent, NavPedirTurnoComponent],
+  imports: [RouterModule,CommonModule, WidgetBienvenidaComponent, ModalComponent, ElegirNegocioComponent, NavPedirTurnoComponent],
   templateUrl: './dashboard-cliente.component.html',
   styleUrl: './dashboard-cliente.component.css'
 })
 export class DashboardClienteComponent implements OnInit{
 
+  @ViewChild('navContainer') navContainer!: ElementRef;
   idCliente: number = 0;
 
   //arreglos
@@ -41,16 +43,40 @@ export class DashboardClienteComponent implements OnInit{
   //variables
   modalLevantado:boolean = false;
   clienteActual:ClienteInterface = {} as ClienteInterface;
+  historialLevantado:Boolean = false;
+  constructor(private cdr: ChangeDetectorRef,private router: Router) {}
+
   ngOnInit(): void {
     this.idCliente = this.authService.getIdUsuario()!;
     this.obtenerInfo()
+  }
+  cerrarHistorial(event: Event) {
+    this.historialLevantado = (event.target as HTMLInputElement).checked;
+  }
+  manejarBotonHistorial(event:Boolean){
+      this.historialLevantado = event;
+      if (!this.historialLevantado) {
+        this.router.navigate(['/dashboard-cliente']);
+      }
+  }
+
+  ngAfterViewInit(): void {
+    // Esperamos a que se renderice el nav y obtenemos su altura
+    const navHeight = this.navContainer.nativeElement.offsetHeight;
+    // Actualizamos la variable CSS global --nav-height
+    document.documentElement.style.setProperty('--nav-height', `${navHeight}px`);
+  }
+
+  updateNavHeight(): void {
+    const navHeight = this.navContainer.nativeElement.offsetHeight;
+    // Actualizamos la variable CSS --nav-height en el root
+    document.documentElement.style.setProperty('--nav-height', `${navHeight}px`);
   }
 
 
 
   listadoTurnos:TurnoInterface[] = [];
 
-  constructor(private cdr: ChangeDetectorRef) {}
 
   obtenerInfo() {
     this.servicioNegocio.getTodosLosNegocios().subscribe({
