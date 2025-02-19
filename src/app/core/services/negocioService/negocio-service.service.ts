6c4be69e4665c6fe01599de77e64f94ad04aa1c5
Leo-Dev <leonardocaimmi1@gmail.com>
2025-02-19 12:35:11 -0300

    patch alta/baja metodo de pago

--- src/app/core/services/negocioService/negocio-service.service.ts
+++ src/app/core/services/negocioService/negocio-service.service.ts
@@ -1,5 +1,5 @@
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { NegocioInterface } from '../../interfaces/negocio-interface';
 import { TablaClientesItem } from '../../../shared/components/tabla-clientes/tabla-clientes-datasource';
@@ -35,5 +35,14 @@ export class NegocioServiceService {
 
   public putNegocio(id:number, negocio:NegocioInterface): Observable<NegocioInterface>{
     return this.http.put<NegocioInterface>(`${this.urlBase}/${id}`, negocio);
+  }
+  public patchDarDeAltaMetodoDePago(idNegocio: number, metodoDePagoId: number): Observable<NegocioInterface> {
+    const params = new HttpParams().set('metodoDePagoId', metodoDePagoId.toString());
+    return this.http.patch<NegocioInterface>(`${this.urlBase}/${idNegocio}/metodos-de-pago/alta`, null, { params });
+  }
+
+  public patchDarDeBajaMetodoDePago(idNegocio: number, metodoDePagoId: number): Observable<NegocioInterface> {
+    const params = new HttpParams().set('metodoDePagoId', metodoDePagoId.toString());
+    return this.http.patch<NegocioInterface>(`${this.urlBase}/${idNegocio}/metodos-de-pago/baja`, null, { params });
   }
 }
