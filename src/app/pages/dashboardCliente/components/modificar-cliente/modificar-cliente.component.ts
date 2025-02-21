f57c718b6906ebe567719cd19f10f6d65cd9a36e
facundoprocelli <128741375+facundoprocelli@users.noreply.github.com>
2025-02-21 19:01:07 -0300

    Configuración del cliente
    El cliente ya puede configurar sus datos de manera correcta
    Adapto los estilos y las funcionalidades

--- src/app/pages/dashboardCliente/components/modificar-cliente/modificar-cliente.component.ts
+++ src/app/pages/dashboardCliente/components/modificar-cliente/modificar-cliente.component.ts
@@ -1,7 +1,14 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { ClienteInterface } from '../../../../core/interfaces/cliente-interface';
 import { CredencialInterface } from '../../../../core/interfaces/credencial.interface';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from '../../../../core/guards/auth/service/auth.service';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
@@ -10,22 +17,40 @@ import { MatIconModule } from '@angular/material/icon';
 import { ICONOS } from '../../../../shared/models/iconos.constants';
 import { ROLES } from '../../../../shared/models/rolesUsuario.constants';
 import { PLACEHOLDERS } from '../../../../shared/models/placeholderInicioSesion.constants';
-import { provideNativeDateAdapter } from '@angular/material/core';
+import {
+  MatPseudoCheckbox,
+  provideNativeDateAdapter,
+} from '@angular/material/core';
 import { ClienteService } from '../../../../core/services/clienteService/cliente.service';
+import { BotonComponent } from '../../../../shared/components/boton/boton.component';
+import {
+  MatCheckboxChange,
+  MatCheckboxModule,
+} from '@angular/material/checkbox';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-modificar-cliente',
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [ReactiveFormsModule, MatIconModule, MatFormFieldModule, MatInputModule, FormsModule, MatDatepickerModule],
+  imports: [
+    ReactiveFormsModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatDatepickerModule,
+    BotonComponent,
+  ],
   templateUrl: './modificar-cliente.component.html',
-  styleUrl: './modificar-cliente.component.css'
+  styleUrl: './modificar-cliente.component.css',
 })
-export class ModificarClienteComponent   {
+export class ModificarClienteComponent implements OnInit {
   //servicios
-  fb: FormBuilder = inject(FormBuilder)//Forms reactives
-  auth: AuthService = inject(AuthService);//Para poder loguear al usuario
-  clienteService: ClienteService = inject(ClienteService);//Para poder modificar al cliente
+  fb: FormBuilder = inject(FormBuilder); //Forms reactives
+  auth: AuthService = inject(AuthService); //Para poder loguear al usuario
+  clienteService: ClienteService = inject(ClienteService); //Para poder modificar al cliente
   //variables
   iconos = ICONOS;
   roles = ROLES;
@@ -37,10 +62,7 @@ export class ModificarClienteComponent   {
   formularioRegister: FormGroup = this.fb.nonNullable.group({
     nombre: new FormControl(''),
     apellido: new FormControl(''),
-    emailRegister: new FormControl('', [ Validators.email]),
     fechaNacimiento: new FormControl(''),
-    //telefono: new FormControl('', Validators.required),
-    //passwordRegister: new FormControl('', Validators.required),
   });
 
   obtenerFormRegister(): ClienteInterface {
@@ -51,44 +73,62 @@ export class ModificarClienteComponent   {
       estado: true,
     };
     return {
-      nombre: this.capitalizarString(this.formularioRegister.get('nombre')?.value),
-      apellido: this.capitalizarString(this.formularioRegister.get('apellido')?.value),
+      nombre: this.capitalizarString(
+        this.formularioRegister.get('nombre')?.value
+      ),
+      apellido: this.capitalizarString(
+        this.formularioRegister.get('apellido')?.value
+      ),
       fechaNacimiento: this.formularioRegister.get('fechaNacimiento')?.value,
       credencial: credencial,
       rolUsuario: 'CLIENTE',
-
     };
-
   }
 
- filtrarDatos(cliente: Partial<ClienteInterface>): Partial<ClienteInterface> {
-  return {
-    ...(cliente.credencial?.email && {// Utilizamos spreed operator junto al && para que luego sobreescribimos el email que se encuentra en credencial y no chillen las interfaces por falta de recursos
-      credencial: {
-        ...cliente.credencial,
-        email: cliente.credencial.email,
-      },
-    }),
-    ...(cliente.nombre && {
-      nombre: cliente.nombre,
-      apellido: cliente.apellido,
-      fechaNacimiento: cliente.fechaNacimiento,
-    }),
-  };
-}
+  filtrarDatos(cliente: Partial<ClienteInterface>): Partial<ClienteInterface> {
+    return {
+      ...(cliente.credencial?.email && {
+        // Utilizamos spreed operator junto al && para que luego sobreescribimos el email que se encuentra en credencial y no chillen las interfaces por falta de recursos
+        credencial: {
+          ...cliente.credencial,
+          email: cliente.credencial.email,
+        },
+      }),
+      ...(cliente.nombre && {
+        nombre: cliente.nombre,
+        apellido: cliente.apellido,
+        fechaNacimiento: cliente.fechaNacimiento,
+      }),
+    };
+  }
+
+  manejarEventoCheckBoxCorreos(event: MatCheckboxChange) {
+    if (event.checked) {
+      // Lógica cuando está marcado
+      console.log('El usuario quiere recibir correos.');
+    } else {
+      // Lógica cuando está desmarcado
+      console.log('El usuario NO quiere recibir correos.');
+    }
+  }
 
   modificarCliente() {
-    if(this.formularioRegister.valid){
+    if (this.formularioRegister.valid) {
       const clienteModificado = this.filtrarDatos(this.obtenerFormRegister());
-      this.clienteService.patchCliente(clienteModificado,this.auth.getIdUsuario()!).subscribe({
-        next: (clienteResponse: ClienteInterface) => {
-          console.dir(clienteResponse);
-        },
-        error: (error: any) => {
-          throw new Error('Error al modificar el cliente');
-        }
-      });
-
+      this.clienteService
+        .patchCliente(clienteModificado, this.auth.getIdUsuario()!)
+        .subscribe({
+          next: (clienteResponse: ClienteInterface) => {
+            Swal.fire({
+              title: 'Datos actualizados con exito!',
+              icon: 'success',
+              confirmButtonText: 'Ok',
+            });
+          },
+          error: (error: any) => {
+            throw new Error('Error al modificar el cliente');
+          },
+        });
     }
   }
   obtenerCliente() {
@@ -98,47 +138,40 @@ export class ModificarClienteComponent   {
       },
       error: (error: any) => {
         throw new Error('Error al obtener el cliente');
-      }
+      },
+    });
+  }
 
-    })
-  };
+  ngOnInit(): void {
+    this.obtenerCliente();
+  }
 
   establecerDatosCliente(cliente: ClienteInterface) {
-    this.obtenerCliente();
     this.formularioRegister.patchValue({
       nombre: this.auth.getNombreUsuario(),
       apellido: cliente.apellido,
-      emailRegister: this.auth.getEmailUsuario(),
-      //telefono: 'Ingrese su nuevo numero de telefono',
       fechaNacimiento: cliente.fechaNacimiento,
-      //passwordRegister: 'Ingrese su nueva contraseña',
     });
-
   }
 
   //validaciones campos formularios
   emailExiste: boolean = false;
   telefonoExiste: boolean = false;
 
-
-
   capitalizarString(palabraFormatear: string): string {
-    return palabraFormatear.charAt(0).toUpperCase() + palabraFormatear.slice(1).toLowerCase();// 0 es la primera letra de la palabra y el resto es el resto de la palabra
+    return (
+      palabraFormatear.charAt(0).toUpperCase() +
+      palabraFormatear.slice(1).toLowerCase()
+    ); // 0 es la primera letra de la palabra y el resto es el resto de la palabra
   }
   tieneErrorRegister(control: string, error: string) {
-    return this.formularioRegister.controls[control].hasError(error) && this.formularioRegister.controls[control].touched;
+    return (
+      this.formularioRegister.controls[control].hasError(error) &&
+      this.formularioRegister.controls[control].touched
+    );
   }
 
-  //metodos para ocultar las contraseñas
-
-  /*ocultarContrasenia = signal(true);
-  ocultarContraseniaEvent(event: MouseEvent) {
-    this.ocultarContrasenia.set(!this.ocultarContrasenia());
-    event.stopPropagation();
-  }*/
-
   mostrarMensajeError(error: string) {
-
     switch (error) {
       case 'required':
         return 'Campo requerido';
@@ -157,7 +190,5 @@ export class ModificarClienteComponent   {
       default:
         return 'Error';
     }
-
   }
-
 }
