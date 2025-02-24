c4b6a54e0c625858d51dfbfa5495af59f5f502be
Leo-Dev <leonardocaimmi1@gmail.com>
2025-02-24 11:15:58 -0300

    Ruta especifica del negocio
    Si la ruta pertenece a un negocio y es para pedir un turno lo que hago es mandarlo a pedir turno al cliente

--- src/app/core/guards/auth/guard/auth.guard-fn.ts
+++ src/app/core/guards/auth/guard/auth.guard-fn.ts
@@ -1,18 +1,21 @@
 import { inject } from "@angular/core";
-import { Router } from "@angular/router";
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from "@angular/router";
 import { AuthService } from "../service/auth.service";
 
 
-export const authGuardFn = () => {
-
+export const authGuardFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
   const authService = inject(AuthService);
-  const route = inject(Router);
-  if (authService.estoyLogueado||(localStorage.getItem('idUsuario')&&localStorage.getItem('rolUsuario'))) {
+  const router = inject(Router);
 
+  if (authService.isAuthenticated()) {
     return true;
   } else {
-    route.navigateByUrl('/login');
+    // Si la ruta es negocios/:nombreNegocio/pedir-turno, guardo la ruta para redirigir al usuario luego de loguearse
+    if (state.url.includes('/negocios/') && state.url.includes('/pedir-turno')) {
+      localStorage.setItem('redirectAfterLogin', state.url);
+    }
+
+    router.navigateByUrl('/login');
     return false;
   }
-
-}
+};
--- src/app/core/guards/auth/service/auth.service.ts
+++ src/app/core/guards/auth/service/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import {jwtDecode} from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 import { AuthInterceptor } from '../auth.interceptor';
 
 @Injectable({
@@ -18,7 +18,25 @@ export class AuthService {
   logIn(token: string): void {
     localStorage.setItem('token', token);
     this.estoyLogueado = true;
+
+    //Me fijo si hay una ruta guardada para redirigir al usuario luego de loguearse
+    const redirectURL = localStorage.getItem('redirectAfterLogin');
+    // Si la ruta es negocios/:nombreNegocio/pedir-turno, redirijo al usuario a esa ruta
+    //Usamos setTimeout para que la redirección se haga en el próximo ciclo de detección de cambios y no en el actual (No funciona en el actual)
+    if(redirectURL?.includes('/negocios/') && redirectURL?.includes('/pedir-turno')){
+      setTimeout(() => {
+        this.route.navigateByUrl(redirectURL!).then(success => {
+          if (success) {
+            localStorage.removeItem('redirectAfterLogin');
+          } else {
+            this.route.navigate(['/dashboard-cliente']);
+          }
+        });
+      }, 0);
+    }
+
   }
+
 
   logOut(): void {
     localStorage.clear();
