4dae65932c0f690745cd98d47af0726e5f40d2de
Fabri-dev <fabri.vogol@gmail.com>
2025-02-25 01:59:30 -0300

    update logica dashboard-cliente

--- src/app/pages/dashboardCliente/components/principal-cliente/principal-cliente.component.ts
+++ src/app/pages/dashboardCliente/components/principal-cliente/principal-cliente.component.ts
@@ -1,45 +1,92 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, Input } from '@angular/core';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
-import { NavPedirTurnoComponent } from '../nav-cliente/nav-cliente';
-import { DashboardClienteComponent } from "../../dashboard-cliente.component";
+import { NavClienteComponent } from '../nav-cliente/nav-cliente';
+import { DashboardClienteComponent } from '../../dashboard-cliente.component';
+import { TablaTurnoClienteComponent } from '../tabla-turno-cliente/tabla-turno-cliente.component';
+import { WidgetBienvenidaComponent } from '../widget-bienvenida/widget-bienvenida.component';
+import { ElegirNegocioComponent } from '../elegir-negocio/elegir-negocio.component';
+import { forkJoin } from 'rxjs';
+import { NegocioInterface } from '../../../../core/interfaces/negocio-interface';
+import { TurnoInterface } from '../../../../core/interfaces/turno-interface';
+import { NegocioServiceService } from '../../../../core/services/negocioService/negocio-service.service';
+import { AuthService } from '../../../../core/guards/auth/service/auth.service';
+import { ClienteInterface } from '../../../../core/interfaces/cliente-interface';
+import { ClienteService } from '../../../../core/services/clienteService/cliente.service';
+import { ModalComponent } from '../../../../shared/components/modal/modal.component';
 
 @Component({
   selector: 'app-principal-cliente',
   standalone: true,
-  imports: [NavPedirTurnoComponent, RouterModule, DashboardClienteComponent],
+  imports: [
+    RouterModule,
+    TablaTurnoClienteComponent,
+    WidgetBienvenidaComponent,
+    ElegirNegocioComponent,
+    ModalComponent,
+  ],
   templateUrl: './principal-cliente.component.html',
-  styleUrl: './principal-cliente.component.css'
+  styleUrl: './principal-cliente.component.css',
 })
 export class PrincipalClienteComponent {
 
-
-  //Variables
-
-  historialLevantado:Boolean = false;
-
-  //Injections
-
-  router:Router = inject(Router);
+  //Input y Output
+  @Input() historialLevantado: boolean = false;
 
 
-  cerrarHistorial(event: Event) {
-    this.historialLevantado = (event.target as HTMLInputElement).checked;
-  }
-  manejarBotonHistorial(event:Boolean){
-      this.historialLevantado = event;
-      if (!this.historialLevantado) {
-        this.router.navigate(['/dashboard-cliente']);
-      }
-  }
+  //arreglos
+  listadoNegocios: NegocioInterface[] = [];
+  listadoTurnos: TurnoInterface[] = [];
 
+  //servicios e Injections
+  servicioNegocio: NegocioServiceService = inject(NegocioServiceService);
+  servicioCliente: ClienteService = inject(ClienteService);
+  authService: AuthService = inject(AuthService);
 
-  isRutaBasica()
-  {
-    let rta = false;
-    if(this.router.url == "/dashboard-cliente") {
-      rta = true;
+  //variables
+  clienteActual: ClienteInterface = {} as ClienteInterface;
+  idCliente: number = 0;
+  modalLevantado: boolean = false;
+
+  //constructor y oninit
+  constructor(private cdr: ChangeDetectorRef, private router: Router) {}
+
+  ngOnInit(): void {
+
+    let auxId=this.authService.getIdUsuario();
+    if(auxId){
+      this.idCliente=auxId;
     }
-    return rta;
+    this.obtenerInfo();
+
   }
 
+  //----------------- Obtener Info -----------------
+
+  obtenerInfo() {
+    forkJoin([
+      this.servicioNegocio.getTodosLosNegocios(),
+      this.servicioCliente.getListadoDeTurnosPorIdCliente(this.idCliente),
+      this.servicioCliente.getClienteById(this.idCliente),
+    ]).subscribe({
+      next: ([negocios, turnos, cliente]) => {
+        this.listadoNegocios = [...negocios];
+        this.listadoTurnos = [...turnos];
+        this.clienteActual = cliente;
+        this.cdr.detectChanges();
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+
+  //-------------- Modal -----------------
+
+  abrirModal() {
+    this.modalLevantado = true;
+  }
+
+  cerrarModal() {
+    this.modalLevantado = false;
+  }
 }
